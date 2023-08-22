5a6aa749cd44dfc175eb0bc210e5f0f491d4d6fd
theruhullah <ruhullahpersonal@gmail.com>
2023-08-22 22:29:39 +0800

    c

--- forms/contact.php
+++ forms/contact.php
@@ -1,11 +1,7 @@
 <?php
   $receiving_email_address = 'ruhullahpersonal@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
